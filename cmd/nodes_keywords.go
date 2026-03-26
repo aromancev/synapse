ca04652caa810a9fdcf75972e74161266ac1900d
Andrey Romantsev <andromancev@gmail.com>
2026-03-26 20:33:19 +0100

    fix(cli): default node keywords to get

--- cmd/nodes_keywords.go
+++ cmd/nodes_keywords.go
@@ -3,8 +3,12 @@ package cmd
 import "github.com/spf13/cobra"
 
 var nodesKeywordsCmd = &cobra.Command{
-	Use:   "keywords",
+	Use:   "keywords [node-id]",
 	Short: "Manage node keywords",
+	Args:  cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nodesKeywordsGetCmd.RunE(cmd, args)
+	},
 }
 
 func init() {
